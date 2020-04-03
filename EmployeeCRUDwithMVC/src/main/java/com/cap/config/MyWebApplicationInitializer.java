524ce9f98124e8bc36ffb915f5a19c2586082274
PRADEEP KUMAR <PRADEEP KUMAR@LAPTOP-HJ805BS5>
2020-04-03 15:03:00 +0530

    Employee CRUD added

---
+++ EmployeeCRUDwithMVC/src/main/java/com/cap/config/MyWebApplicationInitializer.java
@@ -0,0 +1,22 @@
+package com.cap.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MyWebApplicationInitializer extends
+AbstractAnnotationConfigDispatcherServletInitializer{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] {MyWebMVCConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] {"/"};
+	}
+}
